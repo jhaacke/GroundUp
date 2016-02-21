7e186cd72bf2ed168ec77da83a238ec3545d4b80
jhaacke <jeff.haacke@gmail.com>
2016-02-21 18:00:45 -0500

    Added Movies.

---
+++ src/GroundUp/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroundUp.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public ICollection<Actor> Actors { get; set; }
+    }
+}
